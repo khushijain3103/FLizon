81cf099c5b8285e3d79bc8a3a7305bc2358ef712
khushijain3103 <khushijain312003@gmail.com>
2023-08-09 18:50:10 +0530

    fixed bugs

--- routes/admin.js
+++ routes/admin.js
@@ -11,16 +11,17 @@ const products = [];
 
 // /admin/add-product => post
 
-router.post('/add-products' , (req, res, next) => {
+router.post('/add-product' , (req, res, next) => {
     products.push({title: req.body.title});
     res.redirect('/'); // Sends a response
 });
 
 // /admin/add-product => get
 
-router.get('/add-products' , (req, res, next) => {
+router.get('/add-product' , (req, res, next) => {
     console.log('In add products middleware!');
-    res.sendFile(path.join(rootDir , 'views' , 'add-product.html')) // Sends a response
+    // res.sendFile(path.join(rootDir , 'views' , 'add-product.html')) // Sends a response
+    res.render('add-product' , {docTitle: 'Add Product' , path: '/admin/add-product'})
 });
 
 exports.routes = router;
