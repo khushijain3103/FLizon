e81a6bf7ce990c49f66c8e948db843ee85eabbc1
khushijain3103 <khushijain312003@gmail.com>
2023-08-22 23:41:51 +0530

    added delete feature

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,7 +29,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {docTitle: 'Your Cart', path: '/cart'});
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for(product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if(cartProductData) {
+                    cartProducts.push({productData: product, qty: cartProductData.qty});
+                }
+            }
+            console.log(cartProducts);
+            res.render('shop/cart', {docTitle: 'Your Cart', path: '/cart', products: cartProducts});
+        });
+    });
+
+  
 };
 
 exports.postCart = (req, res, next) => {
